e0109a2a31ac3439e34cf120b5e8f7c304600bb2
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-06-14 19:46:38 +0530

    Js Exercises Solutions added

--- JavaScript Excersices/08_calculator/calculator.js
+++ JavaScript Excersices/08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let num = 0;
+	for(let i=0;i<array.length;i++){
+    num += array[i];
+  }
+  return num;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let num1 =1;
+  for(let i=0;i<args.length;i++){
+    num1 *= args[i];
+  }
+  return num1 ;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	let num = num1;
+  for(let i=1;i<num2;i++){
+    num*=num1;
+  }
+  return num;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let prod=1;
+  for(let i=1;i<=num1;i++){
+    prod*=i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
